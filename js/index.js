eaec89a2b36bd96ad9c8a81af195b2cb1df04d43
Lex <Lexor.V.A.S@yandex.ru>
2021-08-16 01:18:11 +0300

    slider beginer add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,138 @@
 const swithButton = document.querySelector('.footer__swith-button');
-const rootSection = document.querySelector('.root')
+const rootSection = document.querySelector('.root');
+const nextSliderBtn = document.querySelector('#nextSliderBtn');
+const previousSliderBtn = document.querySelector('#previousSliderBtn');
+const bicyclesList = document.querySelector('.bicycles__type-list');
+const arrayTypeBicycles = [{
+        name: 'Шоссе',
+        discription: 'На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно.',
+        picture1: {
+            src: './images/type-bike__roud.jpg',
+            alt: 'Дорога Шоссе'
+        },
+        picture2: {
+            src: './images/type-bike__roud1.jpg',
+            alt: 'Дорога гравий'
+        },
+        bicycles: [{
+                picrureSrc: './images/bike/bike1.jpg',
+                picrureAlt: 'Велосипед-Чёрный',
+                name: 'Cervelo Caledonia-5',
+                link: 'https://www.sigmasports.com/item/Cervelo/Caledonia-5-Ultegra-Disc-Road-Bike-2021/RDEN'
+            },
+            {
+                picrureSrc: './images/bike/bike2.jpg',
+                picrureAlt: 'Велосипед-фиолетовый',
+                name: 'Cannondale Systemsix Himod',
+                link: 'https://www.sigmasports.com/item/Cannondale/SystemSix-HiMOD-Ultegra-Di2-Disc-Road-Bike-2021/R82J'
+            },
+            {
+                picrureSrc: './images/bike/bike3.jpg"',
+                picrureAlt: 'Велосипед-Синий',
+                name: 'Trek Domane SL-7',
+                link: 'https://www.sigmasports.com/item/Trek/Domane-SL-7-Force-eTap-AXS-Disc-Road-Bike-2021/RULF'
+            }
+        ]
+    },
+    {
+        name: 'Грэвел',
+        discription: 'Грэвел похож на шоссейный велосипед, но конструкция рамы немного отличается, и на нём стоят более широкие покрышки, всё для того чтобы проехать по лёгкому бездорожью.',
+        picture1: {
+            src: './images/type-bike__roud2.jpg',
+            alt: 'Дорога гравий'
+        },
+        picture2: {
+            src: './images/type-bike__roud3.jpg',
+            alt: 'Дорога ТТ'
+        },
+        bicycles: [{
+                picrureSrc: './images/bike/bike4.jpg',
+                picrureAlt: 'Велосипед-Белый',
+                name: 'Cervelo Aspero GRX 810',
+                link: 'https://www.sigmasports.com/item/Cervelo/Aspero-GRX-810-1x-Disc-Gravel-Bike-2021/RJDE'
+            },
+            {
+                picrureSrc: './images/bike/bike5.jpg',
+                picrureAlt: 'Велосипед-синий',
+                name: 'Cannondale Systemsix Himod',
+                link: 'https://www.sigmasports.com/item/Specialized/S-Works-Diverge-Gravel-Bike-2020/NVJ9'
+            },
+            {
+                picrureSrc: './images/bike/bike6.jpg"',
+                picrureAlt: 'Велосипед-хаки',
+                name: 'Cannondale Topstone Lefty 3',
+                link: 'https://www.sigmasports.com/item/Cannondale/Topstone-Carbon-Lefty-3-Disc-Gravel-Road-Bike-2021/PUC8'
+            }
+        ]
+    },
+    {
+        name: 'ТТ',
+        discription: 'ТТ — это велосипед для триатлона или раздельного старта, гооняют на таком велике только по равнинному асфальту, велик очень быстрые и аэродинамичный.',
+        picture1: {
+            src: './images/type-bike__roud3.jpg',
+            alt: 'Дорога ТТ'
+        },
+        picture2: {
+            src: './images/type-bike__roud1.jpg',
+            alt: 'Дорога Шоссе'
+        },
+        bicycles: [{
+                picrureSrc: './images/bike/bike7.jpg',
+                picrureAlt: 'Синий велосипед',
+                name: 'Specialized S-Works Shiv',
+                link: 'https://www.sigmasports.com/item/Specialized/S-Works-Shiv-Disc-Limited-Edition-Triathlon-Bike-2019/K8P9 '
+            },
+            {
+                picrureSrc: './images/bike/bike8.jpg',
+                picrureAlt: 'Велосипед-серый',
+                name: 'BMC Timemachine 01 ONE',
+                link: 'https://www.sigmasports.com/item/BMC/Timemachine-01-One-Force-Disc-TT-Triathlon-Bike-2021/S835'
+            },
+            {
+                picrureSrc: './images/bike/bike9.jpg"',
+                picrureAlt: 'Велосипед-чёрный',
+                name: 'Cervelo P-Series',
+                link: 'https://www.sigmasports.com/item/Cervelo/P-Series-Ultegra-Di2-TT-Triathlon-Bike-2021/RM6Q'
+            }
+        ]
+    },
+];
+
+function addTypeBike(dataArray, index) {
+    const sectionTypeBike = document.querySelector('.type-bike');
+    const sectionBicycles = document.querySelector('.bicycles');
+    const title = sectionTypeBike.querySelector('.title');
+    const subtitle = sectionTypeBike.querySelector('.subtitle');
+    const typeBikePicture1 = sectionTypeBike.querySelector('#typeBikePicture1');
+    const typeBikePicture2 = sectionTypeBike.querySelector('#typeBikePicture2');
+    title.textContent = dataArray[index].name;
+    subtitle.textContent = dataArray[index].discription;
+    typeBikePicture1.setAttribute('src', dataArray[index].picture1.src);
+    typeBikePicture1.setAttribute('alt', dataArray[index].picture1.alt);
+    typeBikePicture2.setAttribute('src', dataArray[index].picture2.src);
+    typeBikePicture2.setAttribute('alt', dataArray[index].picture2.alt);
+    return
+}
+
 
 swithButton.addEventListener('click', function() {
     swithButton.classList.toggle('footer__swith-button_active');
     rootSection.classList.toggle('root__theme_dark');
-})
+});
+let namber = 0
+addTypeBike(arrayTypeBicycles, namber);
+
+nextSliderBtn.addEventListener('click', function() {
+    if (namber < arrayTypeBicycles.length - 1) {
+        namber++
+        addTypeBike(arrayTypeBicycles, namber);
+        return
+    }
+});
+previousSliderBtn.addEventListener('click', function() {
+    if (namber > 0) {
+        namber--
+        addTypeBike(arrayTypeBicycles, namber);
+        return
+    }
+});
