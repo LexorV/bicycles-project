313da8987646645bdf7dc2a472eba6cd962abd1e
Lex <Lexor.V.A.S@yandex.ru>
2021-08-16 22:41:48 +0300

    add js desktop

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const rootSection = document.querySelector('.root');
 const nextSliderBtn = document.querySelector('#nextSliderBtn');
 const previousSliderBtn = document.querySelector('#previousSliderBtn');
 const bicyclesList = document.querySelector('.bicycles__type-list');
+const bicyclesType = document.querySelectorAll('.bicycles__type-link');
 const arrayTypeBicycles = [{
         name: 'Шоссе',
         discription: 'На шоссейном велосипеде можно ездить по асфальту на разных градиентах: будь то горы или равнины. Гонки проходят в командном пелотоне, но тренироваться можно и самостоятельно.',
@@ -13,6 +14,10 @@ const arrayTypeBicycles = [{
         picture2: {
             src: './images/type-bike__roud1.jpg',
             alt: 'Дорога гравий'
+        },
+        icone: {
+            src: './images/icon/icone-type-bike1.svg',
+            alt: 'Дорога спуск'
         },
         bicycles: [{
                 picrureSrc: './images/bike/bike1.jpg',
@@ -27,7 +32,7 @@ const arrayTypeBicycles = [{
                 link: 'https://www.sigmasports.com/item/Cannondale/SystemSix-HiMOD-Ultegra-Di2-Disc-Road-Bike-2021/R82J'
             },
             {
-                picrureSrc: './images/bike/bike3.jpg"',
+                picrureSrc: './images/bike/bike3.jpg',
                 picrureAlt: 'Велосипед-Синий',
                 name: 'Trek Domane SL-7',
                 link: 'https://www.sigmasports.com/item/Trek/Domane-SL-7-Force-eTap-AXS-Disc-Road-Bike-2021/RULF'
@@ -45,6 +50,10 @@ const arrayTypeBicycles = [{
             src: './images/type-bike__roud3.jpg',
             alt: 'Дорога ТТ'
         },
+        icone: {
+            src: './images/icon/icone-type-bike2.svg',
+            alt: 'Дорога Гравий'
+        },
         bicycles: [{
                 picrureSrc: './images/bike/bike4.jpg',
                 picrureAlt: 'Велосипед-Белый',
@@ -58,7 +67,7 @@ const arrayTypeBicycles = [{
                 link: 'https://www.sigmasports.com/item/Specialized/S-Works-Diverge-Gravel-Bike-2020/NVJ9'
             },
             {
-                picrureSrc: './images/bike/bike6.jpg"',
+                picrureSrc: './images/bike/bike6.jpg',
                 picrureAlt: 'Велосипед-хаки',
                 name: 'Cannondale Topstone Lefty 3',
                 link: 'https://www.sigmasports.com/item/Cannondale/Topstone-Carbon-Lefty-3-Disc-Gravel-Road-Bike-2021/PUC8'
@@ -76,6 +85,10 @@ const arrayTypeBicycles = [{
             src: './images/type-bike__roud1.jpg',
             alt: 'Дорога Шоссе'
         },
+        icone: {
+            src: './images/icon/icone-type-bike3.svg',
+            alt: 'Дорога прямая'
+        },
         bicycles: [{
                 picrureSrc: './images/bike/bike7.jpg',
                 picrureAlt: 'Синий велосипед',
@@ -89,7 +102,7 @@ const arrayTypeBicycles = [{
                 link: 'https://www.sigmasports.com/item/BMC/Timemachine-01-One-Force-Disc-TT-Triathlon-Bike-2021/S835'
             },
             {
-                picrureSrc: './images/bike/bike9.jpg"',
+                picrureSrc: './images/bike/bike9.jpg',
                 picrureAlt: 'Велосипед-чёрный',
                 name: 'Cervelo P-Series',
                 link: 'https://www.sigmasports.com/item/Cervelo/P-Series-Ultegra-Di2-TT-Triathlon-Bike-2021/RM6Q'
@@ -100,17 +113,20 @@ const arrayTypeBicycles = [{
 
 function addTypeBike(dataArray, index) {
     const sectionTypeBike = document.querySelector('.type-bike');
-    const sectionBicycles = document.querySelector('.bicycles');
     const title = sectionTypeBike.querySelector('.title');
     const subtitle = sectionTypeBike.querySelector('.subtitle');
     const typeBikePicture1 = sectionTypeBike.querySelector('#typeBikePicture1');
     const typeBikePicture2 = sectionTypeBike.querySelector('#typeBikePicture2');
+    const typeBikeIcon = sectionTypeBike.querySelector('.type-bike__icon-pulse');
     title.textContent = dataArray[index].name;
     subtitle.textContent = dataArray[index].discription;
     typeBikePicture1.setAttribute('src', dataArray[index].picture1.src);
     typeBikePicture1.setAttribute('alt', dataArray[index].picture1.alt);
     typeBikePicture2.setAttribute('src', dataArray[index].picture2.src);
     typeBikePicture2.setAttribute('alt', dataArray[index].picture2.alt);
+    typeBikeIcon.setAttribute('src', dataArray[index].icone.src);
+    typeBikeIcon.setAttribute('alt', dataArray[index].icone.alt);
+
     return
 }
 
@@ -126,13 +142,66 @@ nextSliderBtn.addEventListener('click', function() {
     if (namber < arrayTypeBicycles.length - 1) {
         namber++
         addTypeBike(arrayTypeBicycles, namber);
-        return
-    }
+        if (namber == arrayTypeBicycles.length - 1) {
+            nextSliderBtn.classList.remove('type-bike__button_active');
+            previousSliderBtn.classList.add('type-bike__button_active');
+        } else if (namber != 0) {
+            previousSliderBtn.classList.add('type-bike__button_active');
+        } else { nextSliderBtn.classList.add('type-bike__button_active') }
+    };
+    return
 });
 previousSliderBtn.addEventListener('click', function() {
     if (namber > 0) {
+        nextSliderBtn.classList.add('type-bike__button_active');
         namber--
+        if (namber == 0) {
+            previousSliderBtn.classList.remove('type-bike__button_active');
+        }
         addTypeBike(arrayTypeBicycles, namber);
         return
+    } else if (namber == 1) {
+        nextSliderBtn.classList.add('type-bike__button_active');
+        previousSliderBtn.classList.remove('type-bike__button_active');
+    } else if (namber > arrayTypeBicycles.length - 1) {
+        nextSliderBtn.classList.add('type-bike__button_active');
     }
+});
+
+function choiceTypeBicycles(index) {
+    bicyclesType.forEach((item) => {
+        item.classList.remove('bicycles__type-link_active');
+    })
+    bicyclesType[index].classList.add('bicycles__type-link_active');
+}
+
+function addBike(dataArray, index) {
+    const sectionBicyclesCards = document.querySelectorAll('.bicycles__card');
+    sectionBicyclesCards.forEach((item, i) => {
+        const picture = item.querySelector('.bicycles__card-picture');
+        const text = item.querySelector('.bicycles__card-text');
+        picture.setAttribute('src', dataArray[index].bicycles[i].picrureSrc);
+        picture.setAttribute('alt', dataArray[index].bicycles[i].picrureAlt);
+        text.setAttribute('href', dataArray[index].bicycles[i].link);
+        text.textContent = (dataArray[index].bicycles[i].name);
+        return
+    });
+
+}
+addBike(arrayTypeBicycles, 0);
+choiceTypeBicycles(0);
+
+
+
+bicyclesType[0].addEventListener('click', function() {
+    choiceTypeBicycles(0);
+    addBike(arrayTypeBicycles, 0);
+});
+bicyclesType[1].addEventListener('click', function() {
+    choiceTypeBicycles(1);
+    addBike(arrayTypeBicycles, 1);
+});
+bicyclesType[2].addEventListener('click', function() {
+    choiceTypeBicycles(2);
+    addBike(arrayTypeBicycles, 2);
 });
